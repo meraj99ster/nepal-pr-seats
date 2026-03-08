f13be0fa5255e323cadd01bffe64e2ed6cebf27f
meraj99ster <meraj99ster@gmail.com>
2026-03-08 11:31:48 +0545

    Update pr_seats_app.py

--- pr_seats_app.py
+++ pr_seats_app.py
@@ -267,7 +267,7 @@ def main():
 
     total_allocated = df_all["Seats"].sum()
     st.markdown(
-        f"<p style='margin-top:0.8rem;font-weight:600;'>Total seats allocated (eligible parties only): "
+        f"<p style='margin-top:0.8rem;font-weight:600;'>Total seats allocated: "
         f"<span style='color:#DC143C;'>{total_allocated:,}</span></p>",
         unsafe_allow_html=True,
     )
